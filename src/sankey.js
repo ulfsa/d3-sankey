c068960cc43748aa47e94f734ac585a36037b3a2
Ulf Sahlin <ulsa@s2.se>
2022-03-06 10:10:15 +0100

    computeNodeLayers fixedLayer

--- src/sankey.js
+++ src/sankey.js
@@ -195,13 +195,20 @@ export default function Sankey() {
     const x = max(nodes, d => d.depth) + 1;
     const kx = (x1 - x0 - dx) / (x - 1);
     const columns = new Array(x);
-    for (const node of nodes) {
-      const i = Math.max(0, Math.min(x - 1, Math.floor(align.call(null, node, x))));
-      node.layer = i;
-      node.x0 = x0 + i * kx;
-      node.x1 = node.x0 + dx;
-      if (columns[i]) columns[i].push(node);
-      else columns[i] = [node];
+      for (const node of nodes) {
+        let i = 0;
+        if (node.fixedLayer) {
+            console.log('found fixedLayer ' + node.fixedLayer);
+            i = node.fixedLayer;
+        }
+        else {
+            i = Math.max(0, Math.min(x - 1, Math.floor(align.call(null, node, x))));
+        }
+        node.layer = i;
+        node.x0 = x0 + i * kx;
+        node.x1 = node.x0 + dx;
+        if (columns[i]) columns[i].push(node);
+        else columns[i] = [node];
     }
     if (sort) for (const column of columns) {
       column.sort(sort);
